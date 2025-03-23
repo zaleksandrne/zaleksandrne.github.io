a51b54fc98d5f20366c8e61d0cab51193c9e2c8e
sasha <zaleksandrne@gmail.com>
2025-03-23 18:00:07 +0300

    edit mute

--- src/components/WebRTCApp.tsx
+++ src/components/WebRTCApp.tsx
@@ -1,29 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { MicOff, Mic, Video, VideoOff } from 'lucide-react';
 
 const roomId = 'test-room';
 const userId = Math.random().toString(36).substr(2, 9);
-const ws = new WebSocket(`wss://gorodami.ru/ws/${roomId}/${userId}`);
-// const ws = new WebSocket(`ws://127.0.0.1:8009/ws/${roomId}/${userId}`);
+// const ws = new WebSocket(`wss://gorodami.ru/ws/${roomId}/${userId}`);
+const ws = new WebSocket(`wss://192.168.31.131:8008/ws/${roomId}/${userId}`);
 
-export const WebRTCApp: React.FC = () => {
+const delay = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const WebRTCApp = () => {
   const localVideoRef = useRef<HTMLVideoElement>(null);
   const [remoteStreams, setRemoteStreams] = useState<{ [key: string]: MediaStream }>({});
   const peerConnections = useRef<{ [key: string]: RTCPeerConnection }>({});
   const [isMuted, setIsMuted] = useState(false);
-  const [isVideoOff, setIsVideoOff] = useState(false);
   let localStream: MediaStream;
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
-      localStream = stream;
-      if (localVideoRef.current) {
-        localVideoRef.current.srcObject = stream;
-      }
-      ws.onopen = () => {
-        ws.send(JSON.stringify({ type: 'join', userId }));
-      };
-    });
+    navigator.mediaDevices
+      .getUserMedia({ video: localStorage.getItem('videoState') !== 'false', audio: true })
+      .then((stream) => {
+        localStream = stream;
+        if (localVideoRef.current) {
+          localVideoRef.current.srcObject = stream;
+        }
+        ws.onopen = () => {
+          ws.send(JSON.stringify({ type: 'join', userId }));
+        };
+      });
 
     ws.onmessage = async (event) => {
       const data = JSON.parse(event.data);
@@ -33,17 +36,31 @@ export const WebRTCApp: React.FC = () => {
         // timeout to fix setRemoteDescriptions
         setTimeout(() => {
           createPeerConnection(data.userId, true);
-        }, 300);
+        }, 600);
       } else if (data.type === 'offer' && data.to === userId) {
         await handleOffer(data.offer, data.from);
       } else if (data.type === 'answer' && data.to === userId) {
-        await peerConnections.current[data.from]?.setRemoteDescription(
-          new RTCSessionDescription(data.answer),
-        );
+        const pc = peerConnections.current[data.from];
+        if (pc && pc.signalingState === 'have-local-offer') {
+          try {
+            await pc.setRemoteDescription(new RTCSessionDescription(data.answer));
+          } catch (error) {
+            console.error('Failed to set remote answer:', error);
+          }
+        }
       } else if (data.type === 'candidate' && data.to === userId) {
+        await delay(500);
+
         await peerConnections.current[data.from]?.addIceCandidate(
           new RTCIceCandidate(data.candidate),
         );
+      } else if (data.type === 'user-disconnected' && data.userId !== userId) {
+        // Удаляем видео-поток удалившегося пользователя
+        setRemoteStreams((prev) => {
+          const newStreams = { ...prev };
+          delete newStreams[data.userId];
+          return newStreams;
+        });
       }
     };
 
@@ -84,7 +101,7 @@ export const WebRTCApp: React.FC = () => {
 
     if (initiator) {
       navigator.mediaDevices
-        .getUserMedia({ video: true, audio: true })
+        .getUserMedia({ video: localStorage.getItem('videoState') !== 'false', audio: true })
         .then((stream) => {
           stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream));
           return peerConnection.createOffer();
@@ -122,19 +139,43 @@ export const WebRTCApp: React.FC = () => {
     if (localStream) {
       localStream.getAudioTracks().forEach((track) => (track.enabled = isMuted));
       setIsMuted(!isMuted);
+
+      // Отправляем сообщение о изменении состояния звука
+      ws.send(
+        JSON.stringify({
+          type: 'mute-toggle',
+          userId,
+          isMuted: !isMuted, // Передаем новое состояние
+        }),
+      );
     }
   };
 
   const toggleVideo = () => {
-    if (localStream) {
-      localStream.getVideoTracks().forEach((track) => (track.enabled = isVideoOff));
-      setIsVideoOff(!isVideoOff);
+    const state = localStorage.getItem('videoState');
+
+    // Если видео выключено, включаем его
+    if (state === 'false') {
+      localStorage.setItem('videoState', 'true');
+    } else {
+      // Если видео включено, выключаем его
+      localStorage.setItem('videoState', 'false');
     }
+
+    // Отправляем сообщение о переключении видео
+    ws.send(JSON.stringify({ type: 'user-disconnected', userId }));
+    ws.close();
+
+    // Обновляем страницу после небольшого таймаута, чтобы все изменения вступили в силу
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   };
 
   return (
     <div className="h-screen bg-black flex flex-wrap justify-center items-center gap-4 p-4">
       <div className="relative">
+        <p style={{ color: 'white' }}>ya</p>
         <video
           ref={localVideoRef}
           muted
@@ -153,7 +194,11 @@ export const WebRTCApp: React.FC = () => {
             onClick={toggleVideo}
             className="p-2 bg-gray-800 text-white rounded-full shadow-md hover:bg-gray-700 transition"
           >
-            {isVideoOff ? <VideoOff size={24} /> : <Video size={24} />}
+            {localStorage.getItem('videoState') === 'false' ? (
+              <VideoOff size={24} />
+            ) : (
+              <Video size={24} />
+            )}
           </button>
         </div>
       </div>
